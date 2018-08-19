394eebf2d56f40aa10737e8724a0b5b2ef6f049e
martin <martin@luckycloud.de>
2018-08-19 11:06:20 +0200

    move setup() always to the beginning

--- FasarekCam/FasarekCam.ino
+++ FasarekCam/FasarekCam.ino
@@ -73,6 +73,119 @@ ArduCAM myCAM(OV5642, CS);
 #endif
 
 
+void setup() {
+
+  std::vector<const char *> menu = {"wifi","wifinoscan","info","sep","restart"};
+  wm.setMenu(menu);
+
+  wm.setMinimumSignalQuality(40);
+   // Callbacks that need to be defined before autoconnect to send a message to display (config and save config)
+  wm.setAPCallback(configModeCallback);
+  wm.setSaveConfigCallback(saveConfigCallback);
+  wm.setDebugOutput(true); 
+  wm.autoConnect(configModeAP);
+  
+while (WiFi.status() != WL_CONNECTED) {
+   delay(500);
+   Serial.print(".");
+}
+  Serial.println("Connected. LocalIP: "+ String( WiFi.localIP() ) );
+  
+  uint8_t vid, pid;
+  uint8_t temp;
+#if defined(__SAM3X8E__)
+  Wire1.begin();
+#else
+  Wire.begin();
+#endif
+  Serial.begin(115200);
+  Serial.println(F("ArduCAM Start!"));
+
+  // set the CS as an output:
+  pinMode(CS, OUTPUT);
+
+  // initialize SPI:
+  SPI.begin();
+  SPI.setFrequency(4000000); //4MHz
+
+  //Check if the ArduCAM SPI bus is OK
+  myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
+  temp = myCAM.read_reg(ARDUCHIP_TEST1);
+  if (temp != 0x55) {
+    Serial.println(F("SPI1 interface Error!"));
+    while (1);
+  }
+#if defined (OV2640_MINI_2MP) || defined (OV2640_CAM)
+  //Check if the camera module type is OV2640
+  myCAM.wrSensorReg8_8(0xff, 0x01);
+  myCAM.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
+  myCAM.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
+  if ((vid != 0x26 ) && (( pid != 0x41 ) || ( pid != 0x42 )))
+    Serial.println(F("Can't find OV2640 module!"));
+  else
+    Serial.println(F("OV2640 detected."));
+#elif defined (OV5640_MINI_5MP_PLUS) || defined (OV5640_CAM)
+  //Check if the camera module type is OV5640
+  myCAM.wrSensorReg16_8(0xff, 0x01);
+  myCAM.rdSensorReg16_8(OV5640_CHIPID_HIGH, &vid);
+  myCAM.rdSensorReg16_8(OV5640_CHIPID_LOW, &pid);
+  if ((vid != 0x56) || (pid != 0x40))
+    Serial.println(F("Can't find OV5640 module!"));
+  else
+    Serial.println(F("OV5640 detected."));
+#elif defined (OV5642_MINI_5MP_PLUS) || defined (OV5642_MINI_5MP) || defined (OV5642_MINI_5MP_BIT_ROTATION_FIXED) ||(defined (OV5642_CAM))
+  //Check if the camera module type is OV5642
+  myCAM.wrSensorReg16_8(0xff, 0x01);
+  myCAM.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
+  myCAM.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
+  if ((vid != 0x56) || (pid != 0x42)) {
+    Serial.println(F("Can't find OV5642 module!"));
+  }
+  else
+    Serial.println(F("OV5642 detected."));
+#endif
+
+
+  //Change to JPEG capture mode and initialize the OV2640 module
+  myCAM.set_format(JPEG);
+  myCAM.InitCAM();
+
+#if defined (OV2640_MINI_2MP) || defined (OV2640_CAM)
+  Serial.println("JPEG_Size:"+String(jpegSize));
+  myCAM.OV2640_set_JPEG_size(jpegSize); 
+  
+#elif defined (OV5640_MINI_5MP_PLUS) || defined (OV5640_CAM)
+  myCAM.OV5640_set_JPEG_size(OV5640_320x240);
+#elif defined (OV5642_MINI_5MP_PLUS) || defined (OV5642_MINI_5MP) || defined (OV5642_MINI_5MP_BIT_ROTATION_FIXED) ||(defined (OV5642_CAM))
+  myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);   //VSYNC is active HIGH
+  myCAM.OV5640_set_JPEG_size(OV5642_320x240);
+#endif
+
+  myCAM.clear_fifo_flag();
+
+   // Set up mDNS responder:
+  // - first argument is the domain name, in this example
+  //   the fully-qualified domain name is "esp8266.local"
+  // - second argument is the IP address to advertise
+  //   we send our IP address on the WiFi network
+  if (!MDNS.begin(localDomain)) {
+    Serial.println("Error setting up MDNS responder!");
+    while(1) { 
+      delay(1000);
+    }
+  }
+  // Add service to MDNS-SD
+  MDNS.addService("http", "tcp", 80);
+  
+  Serial.println("mDNS responder started");
+  // Start the server
+  server.on("/capture", HTTP_GET, serverCapture);
+  server.on("/stream", HTTP_GET, serverStream);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println(F("Server started"));
+  }
+
 void start_capture() {
   myCAM.clear_fifo_flag();
   myCAM.start_capture();
@@ -293,114 +406,6 @@ void handleNotFound() {
   server.send(200, "text/html", headers + html);
 
 }
-
-
-void setup() {
-
-  std::vector<const char *> menu = {"wifi","wifinoscan","info","sep","restart"};
-  wm.setMenu(menu);
-
-  wm.setMinimumSignalQuality(40);
-   // Callbacks that need to be defined before autoconnect to send a message to display (config and save config)
-  wm.setAPCallback(configModeCallback);
-  wm.setSaveConfigCallback(saveConfigCallback);
-  wm.setDebugOutput(true); 
-  wm.autoConnect(configModeAP);
-
-  uint8_t vid, pid;
-  uint8_t temp;
-#if defined(__SAM3X8E__)
-  Wire1.begin();
-#else
-  Wire.begin();
-#endif
-  Serial.begin(115200);
-  Serial.println(F("ArduCAM Start!"));
-
-  // set the CS as an output:
-  pinMode(CS, OUTPUT);
-
-  // initialize SPI:
-  SPI.begin();
-  SPI.setFrequency(4000000); //4MHz
-
-  //Check if the ArduCAM SPI bus is OK
-  myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
-  temp = myCAM.read_reg(ARDUCHIP_TEST1);
-  if (temp != 0x55) {
-    Serial.println(F("SPI1 interface Error!"));
-    while (1);
-  }
-#if defined (OV2640_MINI_2MP) || defined (OV2640_CAM)
-  //Check if the camera module type is OV2640
-  myCAM.wrSensorReg8_8(0xff, 0x01);
-  myCAM.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
-  myCAM.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
-  if ((vid != 0x26 ) && (( pid != 0x41 ) || ( pid != 0x42 )))
-    Serial.println(F("Can't find OV2640 module!"));
-  else
-    Serial.println(F("OV2640 detected."));
-#elif defined (OV5640_MINI_5MP_PLUS) || defined (OV5640_CAM)
-  //Check if the camera module type is OV5640
-  myCAM.wrSensorReg16_8(0xff, 0x01);
-  myCAM.rdSensorReg16_8(OV5640_CHIPID_HIGH, &vid);
-  myCAM.rdSensorReg16_8(OV5640_CHIPID_LOW, &pid);
-  if ((vid != 0x56) || (pid != 0x40))
-    Serial.println(F("Can't find OV5640 module!"));
-  else
-    Serial.println(F("OV5640 detected."));
-#elif defined (OV5642_MINI_5MP_PLUS) || defined (OV5642_MINI_5MP) || defined (OV5642_MINI_5MP_BIT_ROTATION_FIXED) ||(defined (OV5642_CAM))
-  //Check if the camera module type is OV5642
-  myCAM.wrSensorReg16_8(0xff, 0x01);
-  myCAM.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
-  myCAM.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
-  if ((vid != 0x56) || (pid != 0x42)) {
-    Serial.println(F("Can't find OV5642 module!"));
-  }
-  else
-    Serial.println(F("OV5642 detected."));
-#endif
-
-
-  //Change to JPEG capture mode and initialize the OV2640 module
-  myCAM.set_format(JPEG);
-  myCAM.InitCAM();
-
-#if defined (OV2640_MINI_2MP) || defined (OV2640_CAM)
-  Serial.println("JPEG_Size:"+String(jpegSize));
-  myCAM.OV2640_set_JPEG_size(jpegSize); 
-  
-#elif defined (OV5640_MINI_5MP_PLUS) || defined (OV5640_CAM)
-  myCAM.OV5640_set_JPEG_size(OV5640_320x240);
-#elif defined (OV5642_MINI_5MP_PLUS) || defined (OV5642_MINI_5MP) || defined (OV5642_MINI_5MP_BIT_ROTATION_FIXED) ||(defined (OV5642_CAM))
-  myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);   //VSYNC is active HIGH
-  myCAM.OV5640_set_JPEG_size(OV5642_320x240);
-#endif
-
-  myCAM.clear_fifo_flag();
-
-   // Set up mDNS responder:
-  // - first argument is the domain name, in this example
-  //   the fully-qualified domain name is "esp8266.local"
-  // - second argument is the IP address to advertise
-  //   we send our IP address on the WiFi network
-  if (!MDNS.begin(localDomain)) {
-    Serial.println("Error setting up MDNS responder!");
-    while(1) { 
-      delay(1000);
-    }
-  }
-  // Add service to MDNS-SD
-  MDNS.addService("http", "tcp", 80);
-  
-  Serial.println("mDNS responder started");
-  // Start the server
-  server.on("/capture", HTTP_GET, serverCapture);
-  server.on("/stream", HTTP_GET, serverStream);
-  server.onNotFound(handleNotFound);
-  server.begin();
-  Serial.println(F("Server started"));
-  }
 
 void loop() {
   server.handleClient();
